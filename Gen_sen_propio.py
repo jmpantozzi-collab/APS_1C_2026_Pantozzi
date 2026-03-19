076c786bec43760463808eafc5bc3760387f552e
jmpantozzi-collab <jmpantozzi@estudiantes.unsam.edu.ar>
2026-03-18 22:35:53 -0300

    Add files via upload

--- Gen_sen_propio.py
+++ Gen_sen_propio.py
@@ -15,7 +15,7 @@ la frecuencia de muestreo del ADC.: fs
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import pdsmodulos as pds
+from scipy import signal as sig
 
 #%% funcion seno
 def mi_funcion_sen (vmax, dc, ff, ph, nn, fs):
@@ -23,13 +23,13 @@ def mi_funcion_sen (vmax, dc, ff, ph, nn, fs):
         xx = dc + vmax * np.sin(2 * np.pi * ff * tt + ph)
         return tt, xx
 
-#%% main
+#%% ejecucion
 vmax=1
 dc=0
-ff=101
+ff=1
 ph=0
-nn=100
-fs=100
+nn=1000
+fs=1000
 
 tt,xx=mi_funcion_sen(vmax,dc,ff,ph,nn,fs)
 
@@ -39,4 +39,59 @@ plt.ylabel("Amplitud [V]")
 plt.title("Señal Senoidal Generada")
 plt.grid(True)
 plt.show()
+
+#%% funcion ruido segun dB pedido
+
+vmax=np.sqrt(2)
+
+tt,xx=mi_funcion_sen(vmax,dc,ff,ph,nn,fs)
+
+Px=np.var(xx)
+SNR=20
+Pr=10**(-SNR/10)
+R= np.random.normal(0, np.sqrt(Pr), 1000)
+
+xxn=xx + R
+
+plt.figure(1)
+plt.clf()
+plt.plot(tt, xxn)
+plt.plot(tt,xx)
+plt.title(f'f0={f0}Hz')
+
+#%% como usar convolucionar, primero creo una delta
+
+n0= 300 #muestras
+dd= np.zeros(nn)
+dd[n0]= 1.
+
+yy= sig.convolve(xx,dd)
+
+#%% autocorrelacion
+
+yyn= (1/nn) * sig.convolve(R, np.flip(R))
+
+#%% cuantizacion
+
+B = 4 #bits
+
+Vfs = 3 #Volts
+
+qq= Vfs / (2**B)
+
+xxq= np.round(xx/qq)*qq
+
+exxq= xxq-xx
+yyn= (1/nn) * sig.convolve(exxq, np.flip(exxq))
+vare=np.var(exxq)
+varteoe=qq**2/12
+print ("Varianza de error: ", vare)
+print ("varianza teorica: ", varteoe)
+plt.figure(2)
+plt.clf()
+plt.plot(tt, xx)
+plt.plot(tt,xxq)
+plt.title('señal cuantizada')
+
+
 
